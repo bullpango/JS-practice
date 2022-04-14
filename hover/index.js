311423a2d3a664e5543796a47ecdab7d45756f81
bullpango <joyiii@naver.com>
2022-04-14 23:42:27 +0900

    hover and after

---
+++ hover/index.js
@@ -0,0 +1,39 @@
+const iconBox = document.getElementsByClassName('bottom');
+const textBox = document.querySelector('.top');
+const mainText = textBox.querySelector('h1')
+const emoticon =  iconBox.item(HTMLCollection);
+
+//이모티콘
+const bag = emoticon.firstElementChild;
+const message = emoticon.lastElementChild;
+const search = emoticon.childNodes[3];
+
+
+//펑션
+
+const superEventHandler ={
+bag: function(event){
+    event.preventDefault();
+    mainText.innerText = "구매하기";
+},
+search: function(event){
+    event.preventDefault();
+    mainText.innerText = "자세히 보기";
+},
+message: function(event){
+    event.preventDefault();
+    mainText.innerText = "문의하기";
+},
+leave: function(event){
+    event.preventDefault();
+    mainText.innerText = "PS5";
+}
+};
+//이벤트
+
+bag.addEventListener("mouseover", superEventHandler.bag);
+message.addEventListener("mouseover",superEventHandler.message);
+search.addEventListener("mouseover",superEventHandler.search);
+emoticon.addEventListener("mouseleave", superEventHandler.leave);
+
+Init();
