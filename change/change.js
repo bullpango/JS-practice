bef2911da2129347b557c56557cc5355818b2cc8
bullpango <joyiii@naver.com>
2022-04-28 01:22:40 +0900

    Update change.js
    내가 이겼어!!!!!!!!!

--- change/change.js
+++ change/change.js
@@ -3,107 +3,148 @@ const doneList = document.querySelector('#done-list');
 const toDoForm =document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector("#todo-form input");
 
-let toDos,dones = [];
+const TODOS = "toDos";
+const DONE = "done";
 
+let toDosBox, doneBox = [];
 
-function saveToDos(){
-    localStorage.setItem("toDos", JSON.stringify(toDos));
+function getObj(){
+    return{
+        Text:toDoInput.value,
+        id: String(Date.now())
+    };
 }
 
-function saveDones(){
-    localStorage.setItem("dones", JSON.stringify(dones));
+function saveToDos(task){
+    toDosBox.push(task);
 }
 
+function addToDone(task){
+    doneBox.push(task);
+}
 
-function deleteTodos(event){
+function addToDo(task){
+    toDosBox.push(task);
+}
+
+function findInTodo(taskId){
+    return toDosBox.find(function(task){
+        return task.id === taskId;
+    });
+}
+
+function findInDone(taskId){
+    return doneBox.find(function(task){
+        return task.id === taskId;
+    });
+}
+
+function removeFromToDo(taskId){
+    toDosBox = toDosBox.filter(function(task){
+        return task.id !== taskId;
+    });
+}
+
+function removeFromDone(taskId){
+    doneBox = doneBox.filter(function(task){
+        return task.id !== taskId;
+    });
+}
+
+function deleteToDos(event){
     const li = event.target.parentElement;
     li.remove();
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-    dones = dones.filter((dones) => dones.id !== parseInt(li.id));
-    saveToDos();
-    saveDones();
+    removeFromToDo(li.id);
+    removeFromDone(li.id);
+    saveState();
 }
 
-function addToDone(toDos){
-    dones.push(toDos);
+function handleDoneClick(event){
+    const li = event.target.parentElement;
+    li.remove();
+    const task = findInTodo(li.id);
+    removeFromToDo(li.id);
+    addToDone(task);
+    paintDone(task);
+    saveState();
 }
 
-
-function changeToDone(event){
-    const btn = event.target.parentNode;
-    btn.remove(btn);
-    addToDone(toDos);
-    paintDoneList(dones);
-
-    saveDones();
+function handleBackClick(event){
+    const li = event.target.parentElement;
+    li.remove();
+    const task = findInDone(li.id);
+    removeFromDone(li.id);
+    addToDo(task);
+    paintToDo(task);
+    saveState();
 }
 
-function returnToDo(event){
-    console.log('click this');
-}
-
-function paintToDo(newTodo){
-    const li = document.createElement("li");
-    li.id = newTodo.id;
+function generalLi(task){
     const span = document.createElement("span");
-    const delButton = document.createElement("button");
-    delButton.innerText ="❌"
-    delButton.addEventListener('click', deleteTodos);
-    const changeButton = document.createElement("button");
-    changeButton.innerText ="✅"
-    changeButton.addEventListener('click', changeToDone);
-    span.innerText = newTodo.Text;
-    li.appendChild(span);
-    li.appendChild(delButton);
-    li.appendChild(changeButton);
-    toDoList.appendChild(li);
-}
-
-function paintDoneList(dones){
+    span.innerText = task.Text;
     const li = document.createElement("li");
-    li.id = dones.id;
-    const span = document.createElement("span");
-    span.innerText = dones.Text;
-    const delButton = document.createElement("button");
-    delButton.innerText ="❌"
-    delButton.addEventListener('click', deleteTodos);
-    const returnButton = document.createElement("button");
-    returnButton.innerText ="🔄"
-    returnButton.addEventListener('click', returnToDo);
-    li.appendChild(span);
-    li.appendChild(delButton);
-    li.appendChild(returnButton);
-    doneList.appendChild(li);
+    li.id = task.id;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText ="❌";
+    deleteBtn.addEventListener("click", deleteToDos);
+    li.append(span, deleteBtn);
+    return li;
 }
 
-function onToDoSubmit(event){
-event.preventDefault();
-const newTodo = toDoInput.value;
-toDoInput.value = "";
-const newToDoObj ={
-    Text:newTodo,
-    id: Date.now(),
-};
-toDos.push(newToDoObj);
-paintToDo(newToDoObj);
-saveToDos();
+function paintToDo(task){
+    const general = generalLi(task);
+    const completeBtn = document.createElement("button");
+    completeBtn.innerText ="✅";
+    completeBtn.addEventListener("click", handleDoneClick);
+    general.append(completeBtn);
+    toDoList.append(general);
+}
+
+function paintDone(task){
+    const general = generalLi(task);
+    const backBtn = document.createElement("button");
+    backBtn.innerText ="🔄";
+    backBtn.addEventListener("click", handleBackClick);
+    general.appendChild(backBtn);
+    doneList.appendChild(general);
+}
+
+function saveState(){
+    localStorage.setItem(TODOS, JSON.stringify(toDosBox));
+    localStorage.setItem(DONE, JSON.stringify(doneBox));
 }
 
 
-toDoForm.addEventListener('submit', onToDoSubmit);
-
-const savedToDos = localStorage.getItem("toDos");
-
-if(savedToDos !== null){
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
+function loadState(){
+    toDosBox = JSON.parse(localStorage.getItem(TODOS))||[];
+    doneBox = JSON.parse(localStorage.getItem(DONE))||[];
 }
 
-const savedDones = localStorage.getItem("dones");
-
-if(savedDones !== null){
-    const parsedDones = JSON.parse(savedDones);
-    dones = parsedDones;
-    parsedDones.forEach(paintDoneList);
+function restoreState() {
+    toDosBox.forEach(function (task){
+        paintToDo(task);
+    });
+    doneBox.forEach(function (task){
+        paintDone(task);
+    });
 }
+
+
+
+function handleForSubmit(event){
+    event.preventDefault();
+    const newToDo = getObj(toDoInput.value);
+    toDoInput.value = "";
+    generalLi(newToDo);
+    paintToDo(newToDo);
+    saveToDos(newToDo);
+    saveState();
+}
+
+function init(){
+    toDoForm.addEventListener("submit", handleForSubmit);
+    loadState();
+    restoreState();
+}
+
+init();
